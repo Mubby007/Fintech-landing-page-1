f9d5aef9fcd0b8550f8c98dfad7550f22fc12840
Mubby007 <Mubarakadebayo06@gmail.com>
2023-06-05 15:09:18 +0100

    monee
    Fintech landing page for monee

---
+++ index.js
@@ -0,0 +1,8 @@
+let accordion = document.getElementsByClassName("responsive")
+
+for (i = 0; i < accordion.length ; i++ ) {
+
+    accordion[i].addEventListener("click", function() {
+        this.classList.toggle("active")
+    })
+}
